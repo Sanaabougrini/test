13efb212ea857252c662ab3781693d83f638f8f4
Sanaa <sanaa.bougrini@gmail.com>
2016-05-31 12:26:22 +0200

    commit

---
+++ src/main/java/MyClass.java
@@ -0,0 +1,13 @@
+import com.google.common.base.Strings;
+
+public class MyClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(Strings.isNullOrEmpty("nonVide"));
+		System.out.println(Strings.isNullOrEmpty(""));
+		System.out.println(Strings.isNullOrEmpty(null));
+		
+	}
+
+}
